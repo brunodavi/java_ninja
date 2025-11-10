c43c649d60c9c46893fa0ab415061ccfbac3f0f5
brunodavi <48166556+brunodavi@users.noreply.github.com>
2025-11-10 15:50:41 -0300

    Correção na porcentagem

--- src/NivelIntermediario/Desafio5/ContaPoupanca.java
+++ src/NivelIntermediario/Desafio5/ContaPoupanca.java
@@ -9,7 +9,7 @@ public class ContaPoupanca extends ContaBancaria {
     public void depositar(double valor) {
         System.out.println("Valor depositado na conta poupança: " + valor);
         System.out.println("Aviso: Saques tem uma taxa de 10%");
-        double taxa = valor * 0.1;
+        double taxa = valor * 0.01;
         this.saldo += valor - taxa;
     }
 }
